c5a4704e21bbe4e9f5a71cb22106dcd5c705b84f
insolor <2442833+insolor@users.noreply.github.com>
2025-11-28 12:03:12 +0300

    Generate audio

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from gtts import gTTS
+from pathlib import Path
 
 st.header("Chinese Number Trainer")
+
+filename = Path("audio/audio.mp3")
+filename.parent.mkdir(parents=True, exist_ok=True)
+
+text = st.text_input(label="Text")
+button = st.button("Generate")
+
+language = "zh"
+
+if button:
+    tts = gTTS(text, lang=language)
+    tts.save(filename)
+    st.audio(filename, format="audio/mpeg", autoplay=True)
