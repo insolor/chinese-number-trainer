71e028feffd2784bb6bbc74eda482567c3fb3d69
insolor <2442833+insolor@users.noreply.github.com>
2025-11-28 12:14:41 +0300

    Generate unique file names, remove them after playing

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+from uuid import uuid4
 import streamlit as st
 from gtts import gTTS
 from pathlib import Path
 
 st.header("Chinese Number Trainer")
 
-filename = Path("audio/audio.mp3")
-filename.parent.mkdir(parents=True, exist_ok=True)
+audio_directory = Path("audio")
+audio_directory.mkdir(parents=True, exist_ok=True)
 
 text = st.text_input(label="Text")
 button = st.button("Generate")
@@ -13,6 +14,8 @@ button = st.button("Generate")
 language = "zh"
 
 if button:
+    file = audio_directory / f"{uuid4()}.mp3"
     tts = gTTS(text, lang=language)
-    tts.save(filename)
-    st.audio(filename, format="audio/mpeg", autoplay=True)
+    tts.save(file)
+    st.audio(file, format="audio/mpeg", autoplay=True)
+    file.unlink()
