412c9ececff7fe7f1dc30f0288e3f279ee4d8051
insolor <2442833+insolor@users.noreply.github.com>
2025-12-16 12:33:17 +0300

    ruff format

--- tts.py
+++ tts.py
@@ -6,6 +6,7 @@ language = "zh"
 audio_directory = Path("audio")
 audio_directory.mkdir(parents=True, exist_ok=True)
 
+
 def create_audio(text: str) -> Path:
     file = audio_directory / f"{uuid4()}.mp3"
     tts = gTTS(text, lang=language)
