d11631886c963fe8976b2473e6750724ae8a2b63
insolor <2442833+insolor@users.noreply.github.com>
2025-12-16 12:53:43 +0300

    Create run_quiz function

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ from tts import create_audio
 import streamlit as st
 import cn2an
 
-st.header("Chinese Number Trainer")
 
-start_button = st.button(label="Start")
-
-if start_button:
+def run_quiz() -> None:
     number = random.randint(0, 99)
     converted = None
     try:
         converted = cn2an.an2cn(number, "low")
     except ValueError:
-        md = st.error(f"Invalid number: {number}")
+        st.error(f"Invalid number: {number}")
 
     if converted:
-        md = st.markdown(converted)
+        st.markdown(converted)
         file = create_audio(converted)
         st.audio(file, format="audio/mpeg", autoplay=True)
         file.unlink()
+
+
+st.header("Chinese Number Trainer")
+
+start_button = st.button(label="Start")
+if start_button:
+    run_quiz()
